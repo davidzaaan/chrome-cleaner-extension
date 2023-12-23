d9690b576d59161ada2b30d1686b688901acb2c2
David <odmh2711@gmail.com>
2023-12-22 22:38:23 -0500

    Fix extension bug

--- serviceworkers/background.js
+++ serviceworkers/background.js
@@ -1,13 +1,9 @@
-const options = {"origins": []}
-
 chrome.runtime.onStartup.addListener(() => {
   chrome.storage.sync.get(null).then((result) => {
     chrome.browsingData.remove(
-      options,
+      {},
       result, 
-      function () {
-        console.log("Browsing history deleted.");
-      }
-      );
+      () => console.log("Data deleted.")
+    );
   });
 });
